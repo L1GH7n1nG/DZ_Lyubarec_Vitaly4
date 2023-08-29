3089bf62a9a894c5a5079e189dcb17d4962c3daf
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-29 19:24:13 +0300

    Finished

---
+++ zadacha27/Program.cs
@@ -0,0 +1,16 @@
+﻿int SumDigits(int num)
+{
+	if (num < 0) num *= -1;
+	int sum = 0;
+	for (; num > 0;)
+	{
+		int digit = num % 10;
+		sum += digit;
+		num /= 10;
+	}
+	return sum;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} = {SumDigits(number)}");
