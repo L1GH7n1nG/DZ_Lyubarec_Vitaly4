ef85b119eb5f089139b054baa35c4f082ebd8ae2
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-29 19:46:44 +0300

    Finished

---
+++ zadacha29/Program.cs
@@ -0,0 +1,36 @@
+﻿void FillArray(int[] array)
+{
+	Random rnd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rnd.Next(-100, 100);
+	}
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write(array[i]);
+
+		if (i < array.Length - 1)
+		{
+			Console.Write(", ");
+		}
+	}
+	Console.Write(" -> [");
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write(array[i]);
+
+		if (i < array.Length - 1)
+		{
+			Console.Write(", ");
+		}
+	}
+	Console.Write("]");
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
