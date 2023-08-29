45146a40b2d1dd75aeae4bf7c7a7fcd52fa7cd6e
L1GH7n1nG <l1ghtawp1@gmail.com>
2023-08-29 19:05:18 +0300

    Finished

---
+++ zadacha25/Program.cs
@@ -0,0 +1,28 @@
+﻿double Degree(double numA, int numB)
+{
+	double sum = 1;
+	for (int i = 0; i < numB; i++)
+	{
+		sum *= numA;
+	}
+	return sum;
+}
+
+bool Check(int numB)
+{
+	return numB >= 0;
+}
+
+Console.Write("Введите число А: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (Check(numberB))
+{
+	Console.WriteLine($"Результат числа {numberA} в степени {numberB} = {Degree(numberA, numberB)}.");
+}
+else
+{
+	Console.WriteLine("Введено неверное натуральное число B.");
+}
